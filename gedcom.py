9c1873f683271cce69202bd8787921701552b6c3
David Peterson <dfpeterson@gmail.com>
2015-01-02 17:00:54 -0500

    Update gedcom.py

--- gedcom.py
+++ gedcom.py
@@ -1,6 +1,13 @@
 sampleDude = ["0 @P1@ INDI ", "1 BIRT ", "2 DATE 3 November 1957", "2 PLAC Spånga, Stockholm, Sweden", "1 NAME Hans /Lundgren/", "1 SEX M", "1 OBJE", "2 FILE doplp.jpg", "2 FORM jpg", "2 TITL Dolph", "1 OBJE", "2 FILE dolph2.jpg", "2 FORM jpg", "2 TITL profile", "1 OBJE", "2 FILE dolph3", "2 FORM jpg", "2 TITL Ivan Drago", "1 OBJE", "2 FILE dolph4.jpg","2 FORM jpg", "2 TITL Bridge of Dragons", "1 FAMC @F3@", "1 FAMS @F1@"]
 
 class tree():
+    def __init__():
+    
+    def pairnodes(self, person1, person2, relationship):
+        self.nodeID = "abcd"
+        person1.addConnect = self.nodeID
+        person2.addConnect = self.nodeID
+    
     def gedcomTrans(lineofData):
         level = lineofData[:lineofData.find(" ")]
         if lineofData.count(" ") > 1:
